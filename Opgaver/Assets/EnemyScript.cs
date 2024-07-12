1f2c0785ea2996dd357ddffae79b09f3ca5375ee
Deltager <teknisk+GDC2024-GR2@game.unf.dk>
2024-07-12 14:50:22 +0200

    Opg 1

---
+++ Opgaver/Assets/EnemyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Jeg er ond!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
